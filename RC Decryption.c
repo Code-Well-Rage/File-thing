024d37dd49a024232eb8c52699bb6cd6d94ab9b9
Sim Virk <sam.virk25@gmail.com>
2019-04-02 02:01:47 +0000

    Initial functions and variables identified and established, proper code needs to be reviewed and implemented.

---
+++ RC Decryption.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int offsetcalc(int *input);
+
+char decryption[int output];
+
+int main()
+{
+    
+    int offset = 0;
+    char input[];
+    char output[];
+    
+    
+}
+
+int offsetcalc(int *input){
+    
+    
+    
+}
