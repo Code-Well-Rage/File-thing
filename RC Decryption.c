917006c957cfcc5df3cacf7a1e9dad64c444628b
Sim Virk <sam.virk25@gmail.com>
2019-04-09 01:55:47 +0000

    More input, still figuring out how to find the most common letter in a sentence as well as omitting the 'spaces' in the sentence as that interferes with the count.

--- RC Decryption.c
+++ RC Decryption.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 
-int offsetcalc(int *input);
+int offsetcalc(char input);
 
-char decryption_E[int output];
+char decryption_E(char outputF[]);
 
-char decryption_T[int output];
+char decryption_T(char outputF[]);
 
-char decryption_A[int output];
+char decryption_A(char outputF[]);
 
-char decryption_O[int output];
+char decryption_O(char outputF[]);
 
 int main()
 {
     
     int offset = 0;
+    int *input.pt;
     char input[];
     char output[];
-    input[] = 
+    input[] = "exxegoexsrgi" ;
+    *input.pt = input[];
+    
 }
 
-int offsetcalc(int *input){
+int offsetcalc(int input){
     
-    
+    int mostcommon;
+    input = *input.pt
     
 }
