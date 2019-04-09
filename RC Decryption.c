28c930abeb3c8daad00c70aab4b4c3cdf96a959f
Sim Virk <sam.virk25@gmail.com>
2019-04-09 01:33:08 +0000

    More building on the base and the adding of a .c file to decrypt the SC (Substitution Cipher) in.

--- RC Decryption.c
+++ RC Decryption.c
@@ -2,7 +2,13 @@
 
 int offsetcalc(int *input);
 
-char decryption[int output];
+char decryption_E[int output];
+
+char decryption_T[int output];
+
+char decryption_A[int output];
+
+char decryption_O[int output];
 
 int main()
 {
@@ -10,8 +16,7 @@ int main()
     int offset = 0;
     char input[];
     char output[];
-    
-    
+    input[] = 
 }
 
 int offsetcalc(int *input){
