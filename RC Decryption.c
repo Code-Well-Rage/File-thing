28a5439021b486cba69887636ac8a578cadde25c
Sim Virk <sam.virk25@gmail.com>
2019-04-25 11:21:45 +0000

    Basic Rotation Cipher code which just requires some amending of the numbers that decipher it. Copied from a test file that went a bit over bored.

--- RC Decryption.c
+++ RC Decryption.c
@@ -1,36 +1,116 @@
 #include<stdio.h>
-
-int offsetcalc(char input[]);
-
-char decryption_E(char outputF[]);
-
-char decryption_T(char outputF[]);
-
-char decryption_A(char outputF[]);
-
-char decryption_O(char outputF[]);
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
 
 int main()
 {
+    //counting and assigning the length of the encrypted array to the input array
+    char initialInput[] = "exxegoexsrgi";           
+    int length;
+        length = strlen(initialInput);
+        printf("The cypher is %d element/s long\n", length);
+    char input[length];
+    for(int counter = 0 ; counter < length ; counter ++){
+        
+        input[counter] = initialInput[counter];
+        
+    }
     
-    int offsetE = 0;
-    int offsetT = 0;
-    int offsetA = 0;
-    int offsetO = 0;
-    int *inputpt;
-    char input[];
-    char output[];
-    input[] = "exxegoexsrgi" ;
-    *inputpt = input[];
+    char ASCIIvalues[length];
+    /*defines the arrays and identifies the length, provided with the length the letters inputted are converted 
+    into their ASCII decimal numbers*/
+    for(int counter = 0 ; counter < length ; counter ++){
+        
+        ASCIIvalues[counter] = (int)input[counter];
+        
+    }
+    printf("%d\n", ASCIIvalues[0]); //test print, delete later
+    //An if inside a for, inside a for statement used to find any copies of the ASCII letters in the array
+    char repeatcount[length];
     
-}
+    for(int original = 0 ; original < length ; original ++){
+        
+        for(int counter = 0 ; counter < length ; counter ++){
+            
+            if (ASCIIvalues[original] == ASCIIvalues[counter]){
+                
+                repeatcount[original] ++;
+                
+            }
+        
+        }
+        
+    }
+    printf("%d\n", repeatcount[0]); //test print, delete later
+    /*A repeated if statement based off the repition statement above that finds the largest element 
+    and its location, which is then applied back to the original ASCII version of the Cypher to find 
+    the element that will be used to find the offset from E (and then offset from T, A & O)*/
+    int maxcount;
+    maxcount = repeatcount[0];
+    int elementNum;
+    int ogASCIINum;
+    int ASCIIe = 101;
+    int offset;
+    int maxASCIIletter;
+    
+    for(int repeatNum = 0 ; repeatNum < length ; repeatNum ++){
 
-int offsetcalc(char input[]){
+        if (maxcount < repeatcount[repeatNum]){
+            maxcount = repeatcount[repeatNum];
+            elementNum = repeatNum;
+        }
+
+            
+    }
     
-    int mostcommon;
-    int p;
-    input = *inputpt;
+    maxASCIIletter = maxcount;
+    ogASCIINum = ASCIIvalues[elementNum];
+    offset = ASCIIe - ogASCIINum;
+    printf("The most common lettter repeats %d time/s \n", maxASCIIletter);
     
+    char solutionASCII[length];
     
+    if (offset <= 0){
+        
+        for(int n = 0; n < length; n ++){
+            
+            solutionASCII[n] = ASCIIvalues[n] + abs(offset);
+            if (solutionASCII[n] > 122){
+                
+                solutionASCII[n] = solutionASCII[n] - 26;
+                
+            }
+            
+        }
+        
+    }
+    else if (offset > 0){
+        
+         for(int n = 0; n < length; n ++){
+            
+            solutionASCII[n] = ASCIIvalues[n] - abs(offset);
+            if (solutionASCII[n] < 97){
+                
+                solutionASCII[n] = solutionASCII[n] + 26;
+                
+            }
+        
+        }
+
+    }
+    
+    for(int counter = 0 ; counter < length ; counter ++){
+        
+        solutionASCII[counter] = (char)solutionASCII[counter];
+        
+    }
+    
+    printf("The Decrypted Cypher is:\n");
+     for(int counter = 0 ; counter < length ; counter ++){
+        
+        printf("%c", (char)solutionASCII[counter]);
+        
+    }
     
 }
